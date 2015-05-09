b6a66f80ba27b686ba4bf13ec12929be9ede19cc
Javier Gomez <gomosoft@MacBook-Air-de-Viany.local>
2015-05-09 00:54:29 -0500

    latest

--- old/app/controllers/HomeController.js
+++
@@ -1,9 +0,0 @@
-angular.module('DigitalHomes').controller('HomeController', function($rootScope, $state, $modalStack){
-	$rootScope.goToInicio = function(){
-		$state.go('home.inicio');
-	}
-
-	$rootScope.$on("$stateChangeStart", function(){
-		$modalStack.dismissAll();
-	});
-});
--- old/app/services/EdificioService.js
+++
@@ -1,8 +0,0 @@
-angular.module('DigitalHomes').factory('EdificioService', function($http, ConexionService){
-	var url = ConexionService.url;
-	return {
-		getEdificio : function(buildingId){
-			return $http.get(url+'api/Building/'+buildingId);
-		}
-	}
-});
