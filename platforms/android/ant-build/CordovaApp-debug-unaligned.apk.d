fbc7ab81bef2c91810cfdff320cfa02098e86de2
manuel9010 <manuel@nativapps>
2015-05-11 11:49:27 -0500

    latest

--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,8 +0,0 @@
-/home/gomosoft/dev/rtc/dhome-mobile/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/home/gomosoft/dev/rtc/dhome-mobile/platforms/android/ant-build/CordovaApp.ap_ \
-/home/gomosoft/dev/rtc/dhome-mobile/platforms/android/ant-build/classes.dex \
-/home/gomosoft/dev/rtc/dhome-mobile/platforms/android/CordovaLib/ant-build/classes.jar \
-/home/gomosoft/dev/rtc/dhome-mobile/platforms/android/libs/LibRecognizer.jar \
-/home/gomosoft/dev/rtc/dhome-mobile/platforms/android/libs/armeabi/libBlinkBarcode.so \
-/home/gomosoft/dev/rtc/dhome-mobile/platforms/android/libs/x86/libBlinkBarcode.so \
-/home/gomosoft/dev/rtc/dhome-mobile/platforms/android/libs/armeabi-v7a/libBlinkBarcode.so \
